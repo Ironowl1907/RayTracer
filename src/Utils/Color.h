ba8572f31a944a8e0c21cf4bb8383771a9a98747
Ironowl1907 <facuguialtana7@gmail.com>
2025-12-30 14:56:38 +0000

    Correcting gamma

--- src/Utils/Color.h
+++ src/Utils/Color.h
@@ -7,10 +7,20 @@
 
 using Color = Vec3;
 
+inline double linear2Gamma(double linearComponent) {
+  if (linearComponent > 0)
+    return std::sqrt(linearComponent);
+  return 0;
+}
+
 inline void writeColor(std::ostream &out, const Color &pixel_color) {
   auto r = pixel_color.x();
   auto g = pixel_color.y();
   auto b = pixel_color.z();
+
+  r = linear2Gamma(r);
+  g = linear2Gamma(g);
+  b = linear2Gamma(b);
 
   static const Interval intensity(0.000, 0.999);
   int rbyte = int(256 * intensity.clamp(r));
